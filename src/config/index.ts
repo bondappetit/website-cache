50a7601c203ca08a58fdc0083a8291f44ef977fc
artur <amamedbekov@tokenomica.com>
2021-04-10 18:47:47 +0300

    fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,9 +7,10 @@ export default {
       port: parseInt(process.env.WEBSERVER_PORT ?? '8080', 10),
     },
     database: {
+      host: process.env.DATABASE_HOST ?? '',
       user: process.env.DATABASE_USER ?? '',
       password: process.env.DATABASE_PASSWORD ?? '',
-      database: 'bondappetit',
+      database: process.env.DATABASE_NAME ?? '',
     },
     ethereum: {
       host: process.env.ETH_NODE ?? '',
