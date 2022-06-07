9290947888aadea4236b01a3243574fb915b53ab
Vladislav Komissarov <mastyf@gmail.com>
2022-06-07 16:05:08 +0300

    website-cache deploy

--- src/config/index.ts
+++ src/config/index.ts
@@ -12,9 +12,11 @@ export default {
     },
     database: {
       host: process.env.DATABASE_HOST ?? '',
+      port: int(process.env.DATABASE_PORT ?? '5432'),
       user: process.env.DATABASE_USER ?? '',
       password: process.env.DATABASE_PASSWORD ?? '',
       database: process.env.DATABASE_NAME ?? '',
+      ssl: process.env.DATABASE_SSL ?? '',
     },
     ethereum: [
       {
